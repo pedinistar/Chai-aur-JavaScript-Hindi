c47528b34ff77a304a8e015287c5332b5566201a
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-01-19 05:44:48 +0000

    datatypes-summary

---
+++ 01_Basics/05_Datatypes-Summary.js
@@ -0,0 +1,31 @@
+/*
+2 types 
+- Primitive
+- Non-Primitive
+*/
+
+
+/*
+# Primitive (these are call by value datatypes. there are 7 types)
+-- String
+-- Number
+-- Boolean
+-- null
+-- undefined
+-- Symbol
+-- BigInt
+*/
+
+
+/*
+# Reference Type / Non-Primitive Type
+-- Array
+-- Objects
+-- Functions
+*/
+
+
+// Q > js dynamic language hai ya static ?
+// A > 
+
+
