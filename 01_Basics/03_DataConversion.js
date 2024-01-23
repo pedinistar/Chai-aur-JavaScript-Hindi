8d095b5a1b544178fdcd75205db31034705e549d
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-01-23 17:38:27 +0530

    Update 03_DataConversion.js

--- 01_Basics/03_DataConversion.js
+++ 01_Basics/03_DataConversion.js
@@ -1,47 +1,37 @@
-/*
-let score = "33s";    // ----> NaN
-// let score = null;        ---> 0
-
-console.log(typeof score);
-console.log(typeof (score));
-
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);      // NaN -> Not a Number
+let score = "33s";    
+let score2 = null;       
+ 
+console.log(Number(score));    // ----> NaN  [Not a Number]
+console.log(Number(score2));   // ---> 0
 
 
-
-// "33"  =>  33
-// "33ab"  => NaN
-// true => 1
-// false  => 0
-// null => 0
-
+// Number("33")  =>  33
+// Number("33ab")  => NaN
+// Number(true) => 1 ⭐
+// Number(false)  => 0 ⭐
+// Number(null) => 0 ⭐
+// Number(undefined) => NaN⭐
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);   // true
 
 
-//  1 => true
-//  0 => false
-//  "" => false
-// "hitesh" => true
+//  Boolean(1) => true    ⭐
+//  Boolean(0) => false   ⭐
+//  Boolean("") => false  ⭐
+// Boolean("hitesh") => true  ⭐
+// Boolean(undefined) => false⭐
 
 
 
 let someNum = 33;
-
 let stringNum = String(someNum);
-console.log(stringNum);
-console.log(typeof stringNum);
+console.log(stringNum);  // '33'
+console.log(typeof stringNum);  // string
 
-
-*/
 
 // OPERATIONS 
-
 let value = 3;
 let negValue = -value;
 console.log(negValue);    // -3
@@ -54,10 +44,12 @@ let str3 = str1+str2;
 console.log(str3);
 
 
+// 😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵
 console.log("1"+2);     // 12
 console.log(1+"2");     // 12
 console.log("1"+2+2);   // 122
 console.log(1+2+"2");   // 32
+// 😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵
 
 
 let gameCounter = 100;
