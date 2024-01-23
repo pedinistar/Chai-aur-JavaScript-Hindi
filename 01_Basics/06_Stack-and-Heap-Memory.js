46f53394fcd0e93dab2d7fdd615960d8b2da445d
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-01-23 17:40:59 +0530

    Update 06_Stack-and-Heap-Memory.js

--- 01_Basics/06_Stack-and-Heap-Memory.js
+++ 01_Basics/06_Stack-and-Heap-Memory.js
@@ -1,12 +1,8 @@
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-// 2 types of Memory
+// There are two types of memory in JavaScript
 // - Stack (used with Primitive)
 // - Heap (used with Non-Primitive)
 
 let myName = "Jayashree"
-
 let anotherName = myName;
 
 console.log(myName);   // Jayashree
@@ -15,6 +11,11 @@ console.log(anotherName);   // Jayashree
 anotherName = "june uze"
 console.log(myName); // Jayashree
 console.log(anotherName);   // june uze
+
+/* here myName contained "jayashree" then we assigned anotherName as myName thus now both of them contain same string "jayashree"
+   but then when i changed anotherName to "june uze", it didn't changed myName. thus these are primitive datatypes where memory
+   is stored using stack
+*/
 
 
 
@@ -28,7 +29,13 @@ let userTwo = userOne;
 console.log(userOne);   // { email: 'user@gmail.com', upi: 'user@y2' }
 console.log(userTwo);   // { email: 'user@gmail.com', upi: 'user@y2' }
 
-userTwo.email = "user2--@gmail.com"
+userTwo.email = "--@gmail.com"
 
-console.log(userOne);  // { email: 'user2--@gmail.com', upi: 'user@y2' }
+console.log(userOne);  // { email: '--@gmail.com', upi: 'user@y2' }
 console.log(userTwo);  // { email: 'user2--@gmail.com', upi: 'user@y2' }
+
+
+/* here userOne contained { email: 'user@gmail.com', upi: 'user@y2' } then we assigned userTwo as userOne thus now both of them 
+  contain same { email: 'user@gmail.com', upi: 'user@y2' } but then when i changed userTwo.email to  "--@gmail.com", it 
+  changed userOne.email too, thus these are non-primitive datatypes where memory Heap is stored using stack
+*/
