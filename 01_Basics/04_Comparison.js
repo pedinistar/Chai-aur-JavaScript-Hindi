7d2e33c0ff0ebc1830561aef8c68790b680c9cf8
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-01-23 17:39:00 +0530

    Update 04_Comparison.js

--- 01_Basics/04_Comparison.js
+++ 01_Basics/04_Comparison.js
@@ -1,40 +1,44 @@
-// console.log(2>1);
-// console.log(2>=1);
-// console.log(2<1);
-// console.log(2==1);
-// console.log(2!=1);
+console.log(2>1);   // true
+console.log(2>=1);  // true
+console.log(2<1);   // false
+console.log(2==1);  // false
+console.log(2!=1);  // true
 
-// console.log("2">1);     // true  - js automatically converts 2 into number itself
-// console.log("02">1);    // true  
-
-
-// -------------------------------------------------------------------------------------
-
-// TRY TO AVOID THE FOLLOWING TYPE OF CONVERSIONS ⚠️
+console.log("2">1);     // true  - js automatically converts 2 into number itself⭐
+console.log("02">1);    // true  ⭐
 
 
-// console.log(null > 0);
-// console.log(null == 0);   
-// console.log(null >= 0);
+// ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+
+// TRY TO AVOID THE FOLLOWING TYPE OF CONVERSIONS 
 
 
-/*
-The reason is that an equality check == and comparisons ><>=<= work differently
-comparisons convert null to a number, treating it as 0.
-That's why (3) null >= 0 is true and (1) null > 0 is false
+console.log(null == 0);  // false
+console.log(null > 0);   // false
+console.log(null < 0);   // false
+console.log(null >= 0);  // true
+console.log(null <= 0);  // true
+
+
+/* (null==0) gives false on the other hand (null>=0) gives true because , equality check operator (==,===)
+   and comparison operators(>,<,<=,>=) work differently. Comparison operators coverts null to a number,
+   that is treating it as 0⭐ Thst's why null==0 gives false and null>=0 gives true
 */
 
 
-// console.log(undefined == 0);   // false
-// console.log(undefined < 0);   // false
+console.log(undefined == 0);  // false
+console.log(undefined > 0);   // false
+console.log(undefined < 0);   // false
+console.log(undefined >= 0);  // false
+console.log(undefined <= 0);  // false
 
 
-// ---------------------------------------------------------------------------------------
+// ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
+
+
 
 
 // STRICT CHECK
 // ===
 console.log("2"===2);
 // diff data type
-
-
